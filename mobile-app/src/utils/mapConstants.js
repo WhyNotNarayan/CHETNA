5a74875870f6faaf8e42df49ca9a28c76517a365
Narayan Gawade <narayangawade684@gmail.com>
2026-04-29 22:43:47 +0530

    feat: initialize project structure with backend news controller and mobile safety dashboards

---
+++ mobile-app/src/utils/mapConstants.js
@@ -0,0 +1,51 @@
+export const SINDHUDURG_COORDS = {
+  latitude: 16.0270,
+  longitude: 73.6876,
+  latitudeDelta: 0.8,
+  longitudeDelta: 0.8,
+};
+
+export const SINDHUDURG_BOUNDARY = [
+  {latitude: 16.5372, longitude: 73.3020}, {latitude: 16.5448, longitude: 73.3017},
+  {latitude: 16.5551, longitude: 73.3076}, {latitude: 16.5649, longitude: 73.3132},
+  {latitude: 16.5748, longitude: 73.3395}, {latitude: 16.5440, longitude: 73.3568},
+  {latitude: 16.5250, longitude: 73.3766}, {latitude: 16.5247, longitude: 73.4151},
+  {latitude: 16.5091, longitude: 73.4451}, {latitude: 16.5065, longitude: 73.4688},
+  {latitude: 16.5267, longitude: 73.4885}, {latitude: 16.5305, longitude: 73.5002},
+  {latitude: 16.5342, longitude: 73.5180}, {latitude: 16.5341, longitude: 73.5244},
+  {latitude: 16.5309, longitude: 73.5456}, {latitude: 16.5410, longitude: 73.5605},
+  {latitude: 16.5554, longitude: 73.5797}, {latitude: 16.5518, longitude: 73.5922},
+  {latitude: 16.5639, longitude: 73.6032}, {latitude: 16.5868, longitude: 73.6110},
+  {latitude: 16.5913, longitude: 73.6196}, {latitude: 16.5865, longitude: 73.6358},
+  {latitude: 16.5859, longitude: 73.6505}, {latitude: 16.5980, longitude: 73.6555},
+  {latitude: 16.6095, longitude: 73.6665}, {latitude: 16.6217, longitude: 73.6759},
+  {latitude: 16.6347, longitude: 73.6900}, {latitude: 16.6344, longitude: 73.7008},
+  {latitude: 16.6202, longitude: 73.7126}, {latitude: 16.6170, longitude: 73.7331},
+  {latitude: 16.6114, longitude: 73.7469}, {latitude: 16.6206, longitude: 73.7563},
+  {latitude: 16.6348, longitude: 73.7462}, {latitude: 16.6495, longitude: 73.7464},
+  {latitude: 16.6563, longitude: 73.7581}, {latitude: 16.6733, longitude: 73.7663},
+  {latitude: 16.6739, longitude: 73.7760}, {latitude: 16.6718, longitude: 73.7905},
+  {latitude: 16.6653, longitude: 73.8168}, {latitude: 16.6604, longitude: 73.8300},
+  {latitude: 16.6597, longitude: 73.8470}, {latitude: 16.6511, longitude: 73.8618},
+  {latitude: 16.6341, longitude: 73.8624}, {latitude: 16.6191, longitude: 73.8707},
+  {latitude: 16.5959, longitude: 73.8647}, {latitude: 16.5742, longitude: 73.8560},
+  {latitude: 16.4500, longitude: 73.8800}, {latitude: 16.3500, longitude: 73.9500},
+  {latitude: 16.2500, longitude: 73.9900}, {latitude: 16.1500, longitude: 74.0500},
+  {latitude: 16.0500, longitude: 74.1200}, {latitude: 15.9500, longitude: 74.2000},
+  {latitude: 15.8500, longitude: 74.2800}, {latitude: 15.7500, longitude: 74.2800},
+  {latitude: 15.7600, longitude: 74.1500}, // East of Dodamarg
+  {latitude: 15.7200, longitude: 73.9800}, // Ambadgaon
+  {latitude: 15.6844, longitude: 73.9659}, // Dodamarg
+  {latitude: 15.7047, longitude: 73.9555}, // Maneri
+  {latitude: 15.7400, longitude: 73.9100}, // Sasoli
+  {latitude: 15.7567, longitude: 73.8919}, // Netarde
+  {latitude: 15.7620, longitude: 73.8300}, // Sherle
+  {latitude: 15.7500, longitude: 73.7500}, // Satose
+  {latitude: 15.7280, longitude: 73.6930}, // Southernmost tip (Terekhol Mouth)
+  {latitude: 15.7350, longitude: 73.6450}, // Redi Port & Town
+  {latitude: 15.7714, longitude: 73.6713}, // Shiroda Beach
+  {latitude: 15.8500, longitude: 73.6000}, {latitude: 15.9500, longitude: 73.5000},
+  {latitude: 16.0500, longitude: 73.4000}, {latitude: 16.1500, longitude: 73.3500},
+  {latitude: 16.2500, longitude: 73.3200}, {latitude: 16.3500, longitude: 73.3100},
+  {latitude: 16.4500, longitude: 73.3050},
+];
